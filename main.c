3ef6dc456f0f8896b898a203007b37ff81bbd279
s <optimis17@sookmyung.ac.kr>
2020-11-12 15:41:37 +0900

    03

--- main.c
+++ main.c
@@ -5,14 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i=10;
-	int *ptr=&i;
+	int i=100;
+	int *p=&i;
+	int **q=&p;
 	
-	printf("i=%d,pi:%p\n",i,ptr);
-	(*ptr)++;
-	printf("i=%d,pi:%p\n",i,ptr);
-	*ptr++;
-	printf("i=%d,pi:%p\n",i,ptr);
+	*p=200;
+	printf("i=%d, *p=%d, **g=%d\n",i,*p,**q);
+	
+	**q=300;
+	
+	printf("i=%d, *p=%d, **g=%d\n",i,*p,**q);
 	
 	return 0;
 }
